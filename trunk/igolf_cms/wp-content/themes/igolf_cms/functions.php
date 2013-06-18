14459e60c3db97d14cfb1d2e8c0ee128c963dd16
luu.duc.thuy@mdg.vn <luu.duc.thuy@mdg.vn@f8c1fd7f-9a96-653d-8b79-b056c7917db1>
2013-06-18 09:17:09 +0000

    thuy

--- trunk/igolf_cms/wp-content/themes/igolf_cms/functions.php
+++ trunk/igolf_cms/wp-content/themes/igolf_cms/functions.php
@@ -1,5 +1,6 @@
 <?php
 	add_theme_support('post-thumbnails',array('post'));
+	set_post_thumbnail_size(340,180,true);
 	//Register a sidebar
 	if(function_exists('register_sidebar')){
 		register_sidebar(array(
@@ -11,7 +12,17 @@
 			'after_title' => '</h2>',
 		));
 		}
-		
+	//get first images to thumbnail
+	function get_first_image() { 
+	global $post, $posts; $first_img = ''; 
+	ob_start(); ob_end_clean(); 
+	$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+	$first_img = $matches [1] [0]; 
+	if(empty($first_img)){ 
+		$first_img = "http://localhost/blog/wp-content/themes/dudoan/images/img-f.jpg";
+	}
+	return $first_img;
+	}		
 	//register one menu
 	if(function_exists('wp_nav_menu')){
 		
